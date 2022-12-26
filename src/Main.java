4517d7cb7a28117844dcfe008c2b97b6da9a0087
yuliavolkova <uvolkova486@gmail.com>
2022-12-26 15:54:19 +0900

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        int[] reversArray = {1, 2, 3, 4, 5};
+        for (int i = reversArray.length - 1; i >= 0; i--) {
+            System.out.print(reversArray[i]);
+        }
     }
 }
